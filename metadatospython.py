f6022b5528d106ba2f5e262478cb3f89586ec825
Rosa E. Bautista Briones <142864059+Rosa-Bautista@users.noreply.github.com>
2025-03-06 12:45:42 -0600

    Update metadatospython.py

--- metadatospython.py
+++ metadatospython.py
@@ -2,5 +2,11 @@
 
 ##metadatos en archivo json, dsps convertirlos en csv
 #https://github.com/pachterlab/ffq/tree/master
-pip install ffq 
+pip install ffq   ##me marca error en el install
+
+##>>> pip install git+https://github.com/pachterlab/ffq
+  File "<string>", line 1
+    pip install git+https://github.com/pachterlab/ffq
+        ^^^^^^^
+SyntaxError: invalid syntax
  
